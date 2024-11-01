66350b1687c08f199818be913932172a123e08b0
Rajatsingh900k <rajatsingh900k@gmail.com>
2024-11-01 21:03:34 +0530

    added "FizzBuzz" challenge

---
+++ Arrays/Challenges/fizzBuzz.js
@@ -0,0 +1,21 @@
+//very simple program no need to write steps...
+
+
+function fizzBuzz(n){
+    for(let i=1;i<=n;i++){
+        if(i%3===0 && i%5===0){
+            console.log("FizzBuzz");
+        }
+        else if(i%5===0){
+            console.log("Buzz");
+        }
+        else if(i%3===0){
+            console.log("Fizz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+    return;
+}
+fizzBuzz(15);
