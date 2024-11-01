4828210bef0b9e53a5e2475bc855afdfbee880de
Rajatsingh900k <rajatsingh900k@gmail.com>
2024-11-01 15:00:06 +0530

    Added methods: push, get, pop, shift and delete-by-index

--- Arrays/Costum Array/1.js
+++ Arrays/Costum Array/1.js
@@ -1,5 +1,4 @@
 //To create custom array we will be using classes
-
 class MyArray{
     
     //we use the function 'constructor' to define the ctor in js.  
@@ -8,13 +7,61 @@ class MyArray{
         this.data={};
     }
 
-    //push method for this custom array
-    push(value){
-        this.data=value;
+    //push method
+    push(item){
+        this.data[this.length]=item;
         this.length++;
         return this.length;
+    }
+
+    //get method
+    get(index){
+        return this.data[index];
+    }
+
+    //pop method
+    pop(){
+        const itemDeleted=this.data[this.length-1];
+        delete this.data[this.length-1];//using 'delete' keyword to delete the item.
+        this.length--;
+        return itemDeleted;
+    }
+
+    //shift method
+    shift(){
+        //Similar to the shift k-elements of LeetCode.
+        const firstItem=this.data[0];
+        for(let i=0;i<this.length-1;i++){
+            this.data[i]=this.data[i+1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+        return firstItem;
+    }
+    //delete by index
+    deleteByIndex(index){
+        const itemTobeDeleted=this.data[index];
+        delete this.data[index];
+        for(let i=index;i<this.length-1;i++)
+        {
+            this.data[i]=this.data[i+1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+        return itemTobeDeleted;
     }
 }
 
 const myNewArray=new MyArray();//creating instance of the array.
-console.log(myNewArray.push(22));
+
+myNewArray.push('I');
+myNewArray.push('Love');
+myNewArray.push('You');
+myNewArray.push('Anushka');
+// myNewArray.shift();
+myNewArray.deleteByIndex(1);
+for(let i=0;i<myNewArray.length;i++)
+{
+    console.log(myNewArray.get(i));
+}
+console.log(myNewArray) 
