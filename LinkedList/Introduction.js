0b9d02e3a3ce3277883ad1d97b5e5e09cb497aa7
Rajatsingh900k <rajatsingh900k@gmail.com>
2024-11-02 09:33:14 +0530

    LinkedList is incomplete-continue after Sorting

---
+++ LinkedList/Introduction.js
@@ -0,0 +1,7 @@
+//A linked list is a linear data structure where elements, called nodes, are not stored contiguously in memory.
+//Instead, each node contains data and a reference, or link, to the next node in the sequence.
+
+{
+    data:1//data(or value)
+    next:null//reference
+}
