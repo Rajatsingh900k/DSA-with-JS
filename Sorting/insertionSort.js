c68634422cd954986198e1b5cdc2b902be0ae0d4
Rajatsingh900k <rajatsingh900k@gmail.com>
2024-11-02 10:19:23 +0530

    Added 'Insertion Sort'

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -21,7 +21,7 @@ function insertionSort(arr) {
         // Move elements of arr[0...i-1] that are greater than key to one position ahead
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
 
         // Insert the key in its correct position
