bc54b93f9468833f36f95f5ab644419679eee148
Rajatsingh900k <rajatsingh900k@gmail.com>
2024-11-06 09:45:42 +0530

    Completed the basic methods of array

--- Arrays/BasicsAndmethodsOf-Array.js
+++ Arrays/BasicsAndmethodsOf-Array.js
@@ -13,7 +13,8 @@
 //        }
 //
 
-//5.for-of loop: The for-of loop is used to iterate over iterable objects like arrays, strings, or other objects that implement the iterable protocol.
+//5.for-of loop: The for-of loop is used to iterate over iterable objects like arrays, strings, 
+//               or other objects that implement the iterable protocol.
 //for (variable of iterable) {
 //     code to be executed
 //}
@@ -98,7 +99,8 @@ const arr=[1,2,3,'Rajat'];
 
 
 
-//###########################################################//
+
+//############################################################################################################################################//
 
 
 
@@ -148,7 +150,8 @@ const newArr2=arr2.map((numbers)=>{
 
 
 // filter:- The filter() method in JavaScript creates a new array with all elements that pass the test implemented by the provided function. 
-// This method doesn't modify the original array but returns a new array with the elements that meet the condition specified in the callback function
+// This method doesn't modify the original array but returns a new array with the elements that meet the condition specified in the 
+// callback function
 
 //syntax: array.filter(function(currentValue, index, array), thisArg);
 // Return Value:
@@ -169,3 +172,89 @@ const newNumArr=numArr.filter((items)=>{
 
 // printArr(newNumArr,5);
 
+
+//reduce() method
+//The reduce() method in JavaScript executes a reducer function on each element of the array, 
+//resulting in a single output value. It’s used to accumulate values from an array into a single result.
+//syntax: array.reduce(function(accumulator, currentValue, index, array), initialValue);
+// Return Value:
+// The single value that results from the reduction process.
+
+const numbers=[1,2,3,4,5,6,7,8,9,10];
+
+//initially the accuulator has first element of the array(or the element which is pointed by the currentValue)
+const sum=numbers.reduce(function(accumulator,currentValue){
+    return accumulator+currentValue;
+})
+// console.log(`The sum of array is ${sum}`);
+
+
+//concat() method
+//The concat() method in JavaScript is used to merge two or more arrays. 
+//It doesn't modify the original arrays but returns a new array containing elements from the original arrays.
+
+//syntax: const newArray = array1.concat(array2, array3, ..., arrayN);
+// Return Value:
+// A new array that includes the elements of the provided arrays or values in the order they were passed.
+
+//Note: On changing the order of concatanating arrays, the order of resultant array will also get changed
+
+//concatnating two arrays.
+const arr4=[1,2,3];
+const arr5=[4,5,6];
+
+const concatenatedArray=arr4.concat(arr5);
+// console.log(`Result of arr4.concat(arr5):`);
+// printArr(concatenatedArray,1);
+
+const concatenatedArray2=arr5.concat(arr4);
+// console.log(`Result of arr5.concat(arr4):`);
+// printArr(concatenatedArray2,1);
+
+//concatnating multiple arrays.
+
+const arr6=[7,8,9];
+const concatenatedArray3=arr4.concat(arr5,arr6);
+// console.log(`Result of multiple concatanation:`);
+// printArr(concatenatedArray3,1);
+
+
+//slice() method
+// The slice() method in JavaScript is used to extract a portion of an array without modifying the original array. 
+// It returns a shallow copy of a part of the array, starting from a given start index up to, but not including, the end index.
+
+//syntax: array.slice(start, end);
+// Return Value:
+// A new array containing the extracted elements. The original array remains unchanged.
+
+const arrayToBeSliced=[0,1,2,3,4,5,6,7,8,9];
+const slicedArray=arrayToBeSliced.slice(1,9);
+// console.log('Array after slicing: ')
+// printArr(slicedArray,1);
+
+
+
+//splice() method
+//The splice() method in JavaScript is used to add, remove, or replace elements in an array. 
+//Unlike the slice() method, splice() modifies the original array
+
+//syntax: array.splice(start, deleteCount, item1, item2, ..., itemN);
+// Return Value:
+// An array containing the deleted elements, or an empty array if no elements were removed.
+
+const myArr3=[1,2,3,4,5,6,7,8,9];
+
+//removing(or deleting) elements of array using splice:
+
+//const removedElements=myArr3.splice(1,5);
+// console.log('removed elements: ')
+// printArr(removedElements,1);
+// console.log('modified array: ');
+// printArr(myArr3,2);
+
+
+//adding elements using splice() method
+//string index=2, numberOfElementsToBeDeleted=0, rest are the items to be added.
+myArr3.splice(2,0,44,55,66,77,88);
+console.log('Array After adding elements, 44,55,66,77,88 strating at index 2');
+printArr(myArr3,1);
